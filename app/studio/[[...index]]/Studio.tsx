394f154569953c6716190ed9db22e3b7f73de21e
jakubfiglak <jakub.figlak@netguru.pl>
2023-06-23 21:53:45 +0200

    fix: ts error

--- app/studio/[[...index]]/Studio.tsx
+++ app/studio/[[...index]]/Studio.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { NextStudio } from "next-sanity/studio";
+import type { Config } from "sanity";
 
 import config from "@/sanity.config";
 
 export function Studio() {
   //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
-  return <NextStudio config={config} />;
+  return <NextStudio config={config as Config} />;
 }
