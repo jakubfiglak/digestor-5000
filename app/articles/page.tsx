c634a6a869e0448390add3d59e96e203fd413942
jakubfiglak <jakub.figlak@netguru.pl>
2023-06-23 22:42:55 +0200

    feat: basic articles page

---
+++ app/articles/page.tsx
@@ -0,0 +1,47 @@
+import { client } from "@/sanity/client";
+import { groq } from "next-sanity";
+import Link from "next/link";
+import { z } from "zod";
+
+const articlesListQuery = groq`*[_type == "article"] {
+  "id": _id,
+  title,
+  "slug": slug.current,
+}`;
+
+const articlesListSchema = z.array(
+  z.object({
+    id: z.string(),
+    title: z.string(),
+    slug: z.string(),
+  })
+);
+
+async function getArticlesList() {
+  const data = await client.fetch(articlesListQuery);
+  return articlesListSchema.parse(data);
+}
+
+const ArticlesPage = async () => {
+  const articles = await getArticlesList();
+
+  return (
+    <>
+      <h1 className="text-4xl text-center mb-6 font-bold">Articles</h1>
+      <ul>
+        {articles.map(({ id, slug, title }) => (
+          <li key={id}>
+            <Link
+              href={`/articles/${slug}`}
+              className="hover:text-secondary transition-colors"
+            >
+              {title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ArticlesPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <>
       <h1 className="text-6xl font-bold text-center">
         Welcome to
         <br />
@@ -8,6 +8,6 @@ export default function Home() {
           DIGESTOR 5000
         </span>
       </h1>
-    </main>
+    </>
   );
 }
