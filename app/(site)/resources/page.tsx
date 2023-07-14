dd134661da4636e1c25291582028621cca02d322
jakubfiglak <jakub.figlak@netguru.pl>
2023-07-14 13:07:46 +0200

    feat: resources page ui

--- app/(site)/resources/page.tsx
+++ app/(site)/resources/page.tsx
@@ -1,13 +1,24 @@
+import { Link2Icon } from '@radix-ui/react-icons';
 import Link from 'next/link';
 import { groq } from 'next-sanity';
 import { z } from 'zod';
 
+import { Badge } from '@/components/ui/badge';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { client } from '@/sanity/client';
 
 const resourcesListQuery = groq`*[_type == "resource"] {
   "id": _id,
   title,
   "slug": slug.current,
+  description,
   type,
   url,
   "tags": tags[]->{title, "slug": slug.current}
@@ -18,6 +29,7 @@ const resourcesListSchema = z.array(
     id: z.string(),
     title: z.string(),
     slug: z.string(),
+    description: z.string().optional().nullable(),
     type: z.union([
       z.literal('article'),
       z.literal('video'),
@@ -43,27 +55,34 @@ const ResourcesPage = async () => {
       <h1 className="text-secondary mb-6 text-center text-4xl font-bold">
         Resources
       </h1>
-      <ul className="grid grid-cols-4 gap-4">
-        {resources.map(({ id, title, url, tags }) => (
+      <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
+        {resources.map(({ id, title, url, description, tags }) => (
           <li key={id}>
-            <a
-              href={url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="border-text hover:border-primary flex h-full flex-col justify-between rounded-lg border p-3 transition-colors"
-            >
-              <h3 className="mb-3 text-xl font-bold">{title}</h3>
-              <div className="flex flex-wrap gap-2">
+            <Card className="flex h-full flex-col">
+              <CardHeader>
+                <a
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="group"
+                >
+                  <CardTitle className="flex items-center gap-1 transition-colors group-hover:text-orange-500">
+                    {title}
+                    <Link2Icon height={16} width={16} />
+                  </CardTitle>
+                </a>
+              </CardHeader>
+              <CardContent className="flex-grow">
+                <CardDescription>{description}</CardDescription>
+              </CardContent>
+              <CardFooter className="flex flex-wrap gap-2">
                 {tags.map((tag) => (
-                  <span
-                    key={tag.slug}
-                    className="border-secondary rounded-full border px-2 py-1 text-xs opacity-60"
-                  >
+                  <Badge key={tag.slug} variant="outline">
                     {tag.title}
-                  </span>
+                  </Badge>
                 ))}
-              </div>
-            </a>
+              </CardFooter>
+            </Card>
           </li>
         ))}
       </ul>
