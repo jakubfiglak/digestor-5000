d9a2f82e0a18ba0cc0f67d830a318b83addb00e3
jakubfiglak <jakub.figlak@netguru.pl>
2023-07-24 13:40:08 +0200

    feat: add /studio to public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/articles(.*)', '/resources(.*)', '/protected-route'],
+  publicRoutes: [
+    '/',
+    '/articles(.*)',
+    '/resources(.*)',
+    '/studio(.*)',
+    '/protected-route',
+  ],
 });
 
 export const config = {
