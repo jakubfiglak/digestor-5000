f10436563c26e77db83c704a1b23dde4e1d30461
jakubfiglak <jakub.figlak@netguru.pl>
2023-07-24 13:24:13 +0200

    feat: add protected examples

---
+++ app/(site)/protected-page/page.tsx
@@ -0,0 +1,9 @@
+const ProtectedPage = () => {
+  return (
+    <div>
+      <p>This is a protected page</p>
+    </div>
+  );
+};
+
+export default ProtectedPage;
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/articles(.*)', '/resources(.*)'],
+  publicRoutes: ['/', '/articles(.*)', '/resources(.*)', '/protected-route'],
 });
 
 export const config = {
