d20ea4773ddbf793eda41c42e6d9c9b56eeafbaf
jakubfiglak <jakub.figlak@netguru.pl>
2023-07-28 09:39:33 +0200

    feat: extract welcome card

--- app/(site)/submit/resource/page.tsx
+++ app/(site)/submit/resource/page.tsx
@@ -1,15 +1,11 @@
 import { SubmitResourceForm } from '@/modules/resources/components/submit-resource-form';
+import { SubmitResourceWelcomeCard } from '@/modules/resources/components/submit-resource-welcome-card';
 
 const SubmitResourcePage = () => {
   return (
     <>
       <h2 className="my-6 text-center text-4xl font-bold">Submit a resource</h2>
-      <p className="mx-auto mb-6 max-w-md text-center">
-        👋 Hello good person! We appreciate your willingness to submit a
-        resource and contribute to the growth of the Frontend Digest initative.
-        Please fill in the form below.
-      </p>
-
+      <SubmitResourceWelcomeCard className="mx-auto mb-6 max-w-md" />
       <SubmitResourceForm className="mx-auto max-w-sm" />
     </>
   );
---
+++ modules/resources/components/submit-resource-welcome-card.tsx
@@ -0,0 +1,29 @@
+import { auth, currentUser } from '@clerk/nextjs';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+
+type SubmitResourceWelcomeCardProps = {
+  className?: string;
+};
+
+export const SubmitResourceWelcomeCard = async ({
+  className,
+}: SubmitResourceWelcomeCardProps) => {
+  const user = await currentUser();
+
+  return (
+    <Card className={cn('text-center', className)}>
+      <CardHeader>
+        <CardTitle>👋 Hello {user?.firstName ?? 'good person'}!</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>
+          We appreciate your willingness to submit a resource and contribute to
+          the growth of the Frontend Digest initative. Please fill in the form
+          below.
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
