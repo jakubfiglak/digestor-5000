c03ff31ecf4dcbdf78e3bc3e1ebbd605ea9b57d8
jakubfiglak <jakub.figlak@netguru.pl>
2023-07-29 16:15:13 +0200

    feat: handle errors

---
+++ app/(site)/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="mt-6 text-center">
+      <h2 className="mb-6 text-2xl font-bold">
+        Oops, something went wrong! 😿
+      </h2>
+      <p>The page you&apos;re looking for could not be found</p>
+    </div>
+  );
+}
