54ac8c9769cb710657b435203384432af2604792
jakubfiglak <jakub.figlak@netguru.pl>
2023-08-09 19:27:33 +0200

    feat: scaffold article desc card

--- app/(site)/creator/articles/page.tsx
+++ app/(site)/creator/articles/page.tsx
@@ -1,3 +1,4 @@
+import { ScaffoldArticleDescriptionCard } from '@/modules/articles/components/scaffold-article-description-card';
 import { ScaffoldArticleForm } from '@/modules/articles/components/scaffold-article-form';
 
 const SubmitResourcePage = () => {
@@ -6,6 +7,7 @@ const SubmitResourcePage = () => {
       <h2 className="my-6 text-center text-4xl font-bold">
         Scaffold an article
       </h2>
+      <ScaffoldArticleDescriptionCard className="mx-auto mb-6 max-w-md" />
       <ScaffoldArticleForm className="mx-auto max-w-sm" />
     </>
   );
