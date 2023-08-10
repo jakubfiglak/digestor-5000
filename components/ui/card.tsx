b80b41fea536a87e62d5c357b2c49baae2dfcd9c
jakubfiglak <jakub.figlak@netguru.pl>
2023-08-10 15:52:48 +0200

    feat: update ui components to use css variables

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'rounded-xl border border-slate-200 bg-white text-slate-950 shadow dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50',
+      'bg-card text-card-foreground rounded-xl border shadow',
       className
     )}
     {...props}
@@ -47,7 +47,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn('text-sm text-slate-500 dark:text-slate-400', className)}
+    className={cn('text-muted-foreground text-sm', className)}
     {...props}
   />
 ));
