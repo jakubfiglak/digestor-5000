559dc0807f59fdaa95bdf72761a25a149a911977
jakubfiglak <jakub.figlak@netguru.com>
2024-02-12 20:30:01 +0100

    refactor: submit resource link component

--- app/(site)/resources/page.tsx
+++ app/(site)/resources/page.tsx
@@ -1,28 +1,15 @@
-import { currentUser } from '@clerk/nextjs';
-import Link from 'next/link';
-
-import { buttonVariants } from '@/components/ui/button';
 import { getResourcesList } from '@/modules/resources/api';
 import { ResourceCard } from '@/modules/resources/components/resource-card';
+import { SubmitResourceLink } from '@/modules/resources/components/submit-resource-link';
 
 const ResourcesPage = async () => {
-  const [user, resources] = await Promise.all([
-    currentUser(),
-    getResourcesList(),
-  ]);
+  const resources = await getResourcesList();
 
   return (
     <>
       <div className="my-6 text-center">
         <h2 className="mb-6 text-4xl font-bold">Resources</h2>
-        {user && (
-          <Link
-            href="/creator/resources"
-            className={buttonVariants({ variant: 'outline', size: 'lg' })}
-          >
-            Submit a resource
-          </Link>
-        )}
+        <SubmitResourceLink>Submit a resource</SubmitResourceLink>
       </div>
       <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
         {resources.map((resource) => (
